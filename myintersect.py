580477b9459b79717b69614545e633f307b556fe
MateuszRajmundKukawski <mrkukawski@gmail.com>
2015-04-19 18:27:06 +0200

    first commit

---
+++ myintersect.py
@@ -0,0 +1,459 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ MyIntersect
+                                 A QGIS plugin
+ MyIntersect
+                              -------------------
+        begin                : 2015-04-19
+        git sha              : $Format:%H$
+        copyright            : (C) 2015 by MRK
+        email                : mrkukawski@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+from PyQt4.QtCore import QSettings, QTranslator, qVersion, QCoreApplication
+from PyQt4.QtGui import QAction, QIcon
+# Initialize Qt resources from file resources.py
+import resources_rc
+from qgis.core import *
+from qgis.gui import *
+# Import the code for the dialog
+from myintersect_dialog import MyIntersectDialog
+import os.path
+
+
+class MyIntersect:
+    """QGIS Plugin Implementation."""
+
+    def __init__(self, iface):
+        """Constructor.
+
+        :param iface: An interface instance that will be passed to this class
+            which provides the hook by which you can manipulate the QGIS
+            application at run time.
+        :type iface: QgsInterface
+        """
+        # Save reference to the QGIS interface
+        self.iface = iface
+        # initialize plugin directory
+        self.plugin_dir = os.path.dirname(__file__)
+        # initialize locale
+        locale = QSettings().value('locale/userLocale')[0:2]
+        locale_path = os.path.join(
+            self.plugin_dir,
+            'i18n',
+            'MyIntersect_{}.qm'.format(locale))
+
+        if os.path.exists(locale_path):
+            self.translator = QTranslator()
+            self.translator.load(locale_path)
+
+            if qVersion() > '4.3.3':
+                QCoreApplication.installTranslator(self.translator)
+
+        # Create the dialog (after translation) and keep reference
+        self.dlg = MyIntersectDialog()
+
+        # Declare instance attributes
+        self.actions = []
+        self.menu = self.tr(u'&MyIntersect')
+        # TODO: We are going to let the user set this up in a future iteration
+        self.toolbar = self.iface.addToolBar(u'MyIntersect')
+        self.toolbar.setObjectName(u'MyIntersect')
+        self.basePath = '/home/mati/Dokumenty/wtykadane/'
+        self.runButton =  self.dlg.runButton.clicked.connect(self.runApp)
+        self.deleteButton =  self.dlg.deleteButton.clicked.connect(self.del_id)
+
+    # noinspection PyMethodMayBeStatic
+    def tr(self, message):
+        """Get the translation for a string using Qt translation API.
+
+        We implement this ourselves since we do not inherit QObject.
+
+        :param message: String for translation.
+        :type message: str, QString
+
+        :returns: Translated version of message.
+        :rtype: QString
+        """
+        # noinspection PyTypeChecker,PyArgumentList,PyCallByClass
+        return QCoreApplication.translate('MyIntersect', message)
+
+
+    def add_action(
+        self,
+        icon_path,
+        text,
+        callback,
+        enabled_flag=True,
+        add_to_menu=True,
+        add_to_toolbar=True,
+        status_tip=None,
+        whats_this=None,
+        parent=None):
+        """Add a toolbar icon to the toolbar.
+
+        :param icon_path: Path to the icon for this action. Can be a resource
+            path (e.g. ':/plugins/foo/bar.png') or a normal file system path.
+        :type icon_path: str
+
+        :param text: Text that should be shown in menu items for this action.
+        :type text: str
+
+        :param callback: Function to be called when the action is triggered.
+        :type callback: function
+
+        :param enabled_flag: A flag indicating if the action should be enabled
+            by default. Defaults to True.
+        :type enabled_flag: bool
+
+        :param add_to_menu: Flag indicating whether the action should also
+            be added to the menu. Defaults to True.
+        :type add_to_menu: bool
+
+        :param add_to_toolbar: Flag indicating whether the action should also
+            be added to the toolbar. Defaults to True.
+        :type add_to_toolbar: bool
+
+        :param status_tip: Optional text to show in a popup when mouse pointer
+            hovers over the action.
+        :type status_tip: str
+
+        :param parent: Parent widget for the new action. Defaults None.
+        :type parent: QWidget
+
+        :param whats_this: Optional text to show in the status bar when the
+            mouse pointer hovers over the action.
+
+        :returns: The action that was created. Note that the action is also
+            added to self.actions list.
+        :rtype: QAction
+        """
+
+        icon = QIcon(icon_path)
+        action = QAction(icon, text, parent)
+        action.triggered.connect(callback)
+        action.setEnabled(enabled_flag)
+
+        if status_tip is not None:
+            action.setStatusTip(status_tip)
+
+        if whats_this is not None:
+            action.setWhatsThis(whats_this)
+
+        if add_to_toolbar:
+            self.toolbar.addAction(action)
+
+        if add_to_menu:
+            self.iface.addPluginToMenu(
+                self.menu,
+                action)
+
+        self.actions.append(action)
+
+        return action
+
+    def initGui(self):
+        """Create the menu entries and toolbar icons inside the QGIS GUI."""
+
+        icon_path = ':/plugins/MyIntersect/icon.png'
+        self.add_action(
+            icon_path,
+            text=self.tr(u'MyIntersect'),
+            callback=self.run,
+            parent=self.iface.mainWindow())
+
+
+    def unload(self):
+        """Removes the plugin menu item and icon from QGIS GUI."""
+        for action in self.actions:
+            self.iface.removePluginMenu(
+                self.tr(u'&MyIntersect'),
+                action)
+            self.iface.removeToolBarIcon(action)
+        # remove the toolbar
+        del self.toolbar
+
+
+    def run(self):
+        """Run method that performs all the real work"""
+        # show the dialog
+        self.dlg.show()
+        # Run the dialog event loop
+        result = self.dlg.exec_()
+        # See if OK was pressed
+        if result:
+            # Do something useful here - delete the line containing pass and
+            # substitute with your code.
+            pass
+    def runApp(self):
+        self.basePath = '/home/mati/Dokumenty/wtykadane/'
+        #first intersect
+        flayer =self.basePath+'budynki_centroids.shp'
+        slayer =self.basePath+'dzialki.shp'
+        olayer =self.basePath+'wyniki/bud_cen_is_dz.shp'
+        self.run_intersect(flayer, slayer, olayer)
+        self.del_id()
+        flayer = self.basePath+'budynki.shp'
+        slayer = olayer
+        olayer = self.basePath+'wyniki/budynki_id_dzialki.shp'
+        self.run_intersect(flayer, slayer, olayer)
+        flayer = olayer
+        olayer = self.basePath+'wyniki/budynki_id_dzialki_nodes.shp'
+        self.extract_nodes(flayer,olayer )
+        flayer = olayer
+        olayer = self.basePath+"wyniki/buffer_06.shp"
+        self.nodeBuffer(flayer, olayer)
+
+
+        pass
+    def run_intersect(self, baseLayerPath, infoLayerPath, outLayerPath):
+
+
+        self.point_file_path = baseLayerPath
+        self.polygon_file_path = infoLayerPath
+        self.outLayerPath = outLayerPath
+
+        #warstwa uaktualniana
+        #self.point_file_path = '/home/mati/Dokumenty/wtykadane/budynki.shp'
+        #warstwa z danymi do pobrania
+       # self.polygon_file_path = '/home/mati/Dokumenty/wtykadane/wyniki/wynik2.shp'
+        self.point_layer = QgsVectorLayer(self.point_file_path,
+                                          os.path.splitext(self.point_file_path)[0], 'ogr')
+        self.polygon_layer = QgsVectorLayer(self.polygon_file_path,os.path.splitext(self.polygon_file_path)[0], 'ogr')
+        self.polygon_provider =  self.polygon_layer.dataProvider()
+        self.point_provider =  self.point_layer.dataProvider()
+        print('punktow:', self.point_provider.featureCount(),)
+        print('poligonow:', self.polygon_provider.featureCount())
+
+        print 'Kliknieto'
+
+        findex = self.getSpatialIndex(self.point_provider)
+        print(type(findex))
+        findexlist = self.getInsert(findex, self.polygon_provider)
+        print( findexlist,)
+        #fields()
+        fieldList1= self.point_provider.fields()
+        fieldList2 = self.polygon_provider.fields()
+        seq = range(0, len(fieldList1) + len(fieldList2))
+        fieldList1.extend(fieldList2)
+        fieldList1 = dict(zip(seq, fieldList1))
+        fields = QgsFields()
+        for f in fieldList1.values():
+          fields.append(f)
+
+
+
+
+
+
+        index = self.getSpatialIndex(self.polygon_provider)
+        sRs = self.point_provider.crs()
+        writer1 = QgsVectorFileWriter(self.outLayerPath, 'CP1250', fields, self.point_provider.geometryType(),sRs )
+        #writer1 = QgsVectorFileWriter('/home/mati/Dokumenty/wtykadane/wynik2.shp', 'CP1250', fields, self.polygon_provider.geometryType(),sRs )
+
+        inFeat = QgsFeature()
+        outFeat = QgsFeature()
+        inFeatB = QgsFeature()
+        fit1 = self.point_provider.getFeatures()
+        while fit1.nextFeature(inFeat): #dla punktow
+            print 'in while'
+            inGeom = inFeat.geometry()
+            atMap1 = inFeat.attributes()
+            outFeat.setGeometry(inGeom)
+            none = True
+            joinList = []
+            if inGeom.type() == QGis.Point:
+                #(check, joinList) = layer2.featuresInRectangle(inGeom.buffer(10,2).boundingBox(), True, True)
+                #layer2.select(inGeom.buffer(10,2).boundingBox(), False)
+                #joinList = layer2.selectedFeatures()
+                joinList = index.intersects( inGeom.buffer(10,2).boundingBox() )
+                print 'len(joinList)', len(joinList)
+            else:
+                joinList = index.intersects( inGeom.boundingBox() )
+                'len(joinList)', len(joinList)
+
+
+
+            #joinList = index.intersects( inGeom.boundingBox() )
+            #raw_input('nacisjin')
+            for i in joinList:
+               # print 'in joinList for'
+                self.polygon_provider.getFeatures( QgsFeatureRequest().setFilterFid( int(i) ) ).nextFeature( inFeatB )
+                tempGeom = QgsGeometry( inFeatB.geometry() )
+                #print 'in join za self'
+
+                if inGeom.intersects(inFeatB.geometry()):
+                    print 'w if inGeom.intersects(inFeatB.geometry()):'
+                    atMap2 = inFeatB.attributes()
+                    atMap = atMap1
+                    atMap2 = atMap2
+                    atMap.extend(atMap2)
+                    atMap = dict(zip(seq, atMap))
+                    atMap1.extend(atMap2)
+                    print 'in last loop'
+                    #break
+            outFeat.setAttributes(atMap1)
+            writer1.addFeature(outFeat)
+            #writer2.addFeature(outFeat)
+        del writer1
+        #del writer2
+        print 'END'
+        print 'first:',self.point_file_path, 'type:', type(self.point_layer)
+        print 'second', self.polygon_file_path, 'type:', type(self.polygon_layer)
+
+
+
+
+
+
+    def del_id(self):
+        layer_path = self.basePath+'wyniki/bud_cen_is_dz.shp'
+        layer = QgsVectorLayer(layer_path ,os.path.splitext(layer_path)[0], 'ogr')
+        provider = layer.dataProvider()
+        fieldsList = provider.fields()
+        print fieldsList
+        print len(fieldsList)
+        i = 0
+        for field in fieldsList:
+            print 'w petli'
+            print field.name()
+            if field.name()=='id_budynku':
+                del_index = i
+            i+=1
+
+        print(del_index)
+        provider.deleteAttributes([del_index])
+        layer.updateFields()
+
+
+    def getSpatialIndex(self, workLayer):
+        index = QgsSpatialIndex()
+        for feature in workLayer.getFeatures():
+            index.insertFeature(feature)
+        return index
+    def getInsert(self, spatial_index, workLayer):
+        index_list = []
+        for feature in workLayer.getFeatures():
+            #print(feature, type(feature), feature['numer'])
+            geom = QgsGeometry(feature.geometry())
+            print(geom.area(),)
+            index_list.append(spatial_index.intersects(geom.boundingBox()))
+        return index_list
+
+
+    def extract_nodes( self, plygon_layer_path, output_layer_path):
+        layer = QgsVectorLayer(plygon_layer_path,os.path.splitext(plygon_layer_path)[0], 'ogr')
+
+
+        vprovider = layer.dataProvider()
+        writer = QgsVectorFileWriter( output_layer_path, "CP1250", vprovider.fields(),
+                                      QGis.WKBPoint, vprovider.crs() )
+        inFeat = QgsFeature()
+        outFeat = QgsFeature()
+        inGeom = QgsGeometry()
+        outGeom = QgsGeometry()
+        nFeat = vprovider.featureCount()
+        nElement = 0
+        #self.emit( SIGNAL( "runStatus( PyQt_PyObject )" ), 0 )
+        #self.emit( SIGNAL( "runRange( PyQt_PyObject )" ), ( 0, nFeat ) )
+        fit = vprovider.getFeatures()
+        while fit.nextFeature( inFeat ):
+          nElement += 1
+          #self.emit( SIGNAL( "runStatus( PyQt_PyObject )" ),  nElement )
+          inGeom = inFeat.geometry()
+          atMap = inFeat.attributes()
+          pointList = self.extractPoints( inGeom )
+          outFeat.setAttributes( atMap )
+          for i in pointList:
+            outFeat.setGeometry( outGeom.fromPoint( i ) )
+            writer.addFeature( outFeat )
+        del writer
+        return True
+    def extractPoints(self, geom ):
+        multi_geom = QgsGeometry()
+        temp_geom = []
+        if geom.type() == 0: # it's a point
+            if geom.isMultipart():
+                temp_geom = geom.asMultiPoint()
+            else:
+                temp_geom.append(geom.asPoint())
+        if geom.type() == 1: # it's a line
+            if geom.isMultipart():
+                multi_geom = geom.asMultiPolyline() #multi_geog is a multiline
+                for i in multi_geom: #i is a line
+                    temp_geom.extend( i )
+            else:
+                temp_geom = geom.asPolyline()
+        elif geom.type() == 2: # it's a polygon
+            if geom.isMultipart():
+                multi_geom = geom.asMultiPolygon() #multi_geom is a multipolygon
+                for i in multi_geom: #i is a polygon
+                    for j in i: #j is a line
+                        temp_geom.extend( j )
+            else:
+                multi_geom = geom.asPolygon() #multi_geom is a polygon
+                for i in multi_geom: #i is a line
+                    temp_geom.extend( i )
+        return temp_geom
+    def nodeBuffer(self, node_file_path, out_file_path):
+        layer = QgsVectorLayer(node_file_path,os.path.splitext(node_file_path)[0], 'ogr')
+        provider = layer.dataProvider()
+       #writer = QgsVectorFileWriter(self.outLayerPath, 'CP1250', fields, self.point_provider.geometryType(),sRs )
+        fields = provider.fields()
+        writer = QgsVectorFileWriter(out_file_path, "CP1250", fields, QGis.WKBMultiPolygon, provider.crs())
+        #QGis.WKBMultiPolygon
+
+
+
+
+
+        features = provider.getFeatures()
+        new_features = []
+        for feature in features:
+            feature.setGeometry(feature.geometry().buffer(0.6, 20))
+            new_features.append(features)
+            writer.addFeature(feature)
+
+        del writer
+
+
+
+
+    # def chuj(self):
+    #                 """Run method that performs all the real work"""
+    #     # show the dialog
+    #     self.dlg.show()
+    #     # Run the dialog event loop
+    #     result = self.dlg.exec_()
+    #     # See if OK was pressed
+    #     if result:
+    #         # Do something useful here - delete the line containing pass and
+    #         # substitute with your code.
+    #         distance = self.dlg.distance_buf.value()
+    #         new_distance = 0
+    #         number = self.dlg.number_buf.value()
+    #         layer = self.iface.activeLayer()
+    #         selected = layer.selectedFeatures()
+    #         crs = layer.crs()
+    #         print layer.name()
+    #         for i in range(0,number):
+    #             new_distance += distance
+    #             result = QgsVectorLayer("Polygon?crs=" + str(crs.authid()), "result_" + str(new_distance), "memory")
+    #             new_features = []
+    #             result_provider = result.dataProvider()
+    #             for feature in selected:
+    #                 feature.setGeometry(feature.geometry().buffer(new_distance,20))
+    #                 new_features.append(feature)
+    #             result_provider.addFeatures(new_features)
+    #             QgsMapLayerRegistry.instance().addMapLayer(result)
+    #         pass
